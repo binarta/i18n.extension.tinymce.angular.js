65c310d6ae8cdbed95c8fb049c1e7d39977f08c9
Jo Kerkhofs <jke@thinkerit.be>
2013-12-20 14:41:06 +0100

    Updated source and fixed priority bug

--- src/main/js/ui-tinymce.js
+++ src/main/js/ui-tinymce.js
@@ -8,44 +8,58 @@ angular.module('ui.tinymce', [])
         var generatedIds = 0;
         return {
             require: 'ngModel',
+            priority: 1,
             link: function (scope, elm, attrs, ngModel) {
-                var expression, options, tinyInstance;
+                var expression, options, tinyInstance,
+                    updateView = function () {
+                        ngModel.$setViewValue(elm.val());
+                        if (!scope.$root.$$phase) {
+                            scope.$apply();
+                        }
+                    };
                 // generate an ID if not present
                 if (!attrs.id) {
                     attrs.$set('id', 'uiTinymce' + generatedIds++);
                 }
+
+                if (attrs.uiTinymce) {
+                    expression = scope.$eval(attrs.uiTinymce);
+                } else {
+                    expression = {};
+                }
                 options = {
                     // Update model when calling setContent (such as from the source editor popup)
                     setup: function (ed) {
+                        var args;
                         ed.on('init', function(args) {
                             ngModel.$render();
                         });
                         // Update model on button click
                         ed.on('ExecCommand', function (e) {
                             ed.save();
-                            ngModel.$setViewValue(elm.val());
-                            if (!scope.$$phase) {
-                                scope.$apply();
-                            }
+                            updateView();
                         });
                         // Update model on keypress
                         ed.on('KeyUp', function (e) {
-                            console.log(ed.isDirty());
                             ed.save();
-                            ngModel.$setViewValue(elm.val());
-                            if (!scope.$$phase) {
-                                scope.$apply();
+                            updateView();
+                        });
+                        // Update model on change, i.e. copy/pasted text, plugins altering content
+                        ed.on('SetContent', function (e) {
+                            if(!e.initial){
+                                ed.save();
+                                updateView();
                             }
                         });
+                        if (expression.setup) {
+                            scope.$eval(expression.setup);
+                            delete expression.setup;
+                        }
                     },
                     mode: 'exact',
                     elements: attrs.id
                 };
-                if (attrs.uiTinymce) {
-                    expression = scope.$eval(attrs.uiTinymce);
-                } else {
-                    expression = {};
-                }
+                // extend options with initial uiTinymceConfig and options from directive attribute value
                 angular.extend(options, uiTinymceConfig, expression);
                 setTimeout(function () {
                     tinymce.init(options);
