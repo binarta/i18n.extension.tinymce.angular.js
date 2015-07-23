5a8fee4a9d79e483467a9045582b2587f712936d
Jo Kerkhofs <jke@thinkerit.be>
2015-07-23 23:01:14 +0200

    update template installer

--- src/main/js/ui-tinymce.js
+++ src/main/js/ui-tinymce.js
@@ -1,119 +1,69 @@
 angular.module('ui.tinymce', ['i18n', 'image-management', 'notifications', 'toggle.edit.mode', 'angularx', 'checkpoint'])
     .value('uiTinymceConfig', {})
-    .run(['i18nRendererTemplateInstaller', function (i18nRendererTemplateInstaller) {
-        i18nRendererTemplateInstaller(function (editor, isEditable) {
-            switch (editor) {
-                case 'full':
-                    return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
-                        topMenuControls() +
-                        '<textarea ui-tinymce=\"{' +
-                        'plugins: [\'link fullscreen textcolor paste table binartax.link\'],' +
-                        'toolbar: \'undo redo | styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent table | binartax.link | fullscreen\',' +
-                        'theme_advanced_resizing: true,' +
-                        'theme_advanced_resizing_use_cookie : false,' +
-                        'object_resizing: false,' +
-                        'height:\'180\',' +
-                        (isEditable ? '' : 'readonly: 1,') +
-                        'menubar:false}\"' +
-                        'ng-model=\"translation\" name=\"translation\">' +
-                        '</textarea>' +
-                        bottomMenuControls() +
-                        '</form>';
-                case 'media':
-                    return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
-                        topMenuControls() +
-                        '<textarea ui-tinymce=\"{' +
-                        'plugins: [\'fullscreen media paste\'],' +
-                        'toolbar: \'undo redo | media | fullscreen\',' +
-                        'theme_advanced_resizing: true,' +
-                        'theme_advanced_resizing_use_cookie : false,' +
-                        'object_resizing: false,' +
-                        'height:\'180\',' +
-                        (isEditable ? '' : 'readonly: 1,') +
-                        'menubar:false}\"' +
-                        'ng-model=\"translation\" name=\"translation\">' +
-                        '</textarea>' +
-                        bottomMenuControls() +
-                        '</form>';
-                case 'full-media':
-                    return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
-                        topMenuControls() +
-                        '<textarea ui-tinymce=\"{' +
-                        'plugins: [\'link fullscreen binartax.img textcolor paste table binartax.link\'],' +
-                        'toolbar: \'undo redo | styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent table | binartax.link | binartax.img | fullscreen\',' +
-                        'theme_advanced_resizing: true,' +
-                        'theme_advanced_resizing_use_cookie : false,' +
-                        'object_resizing: false,' +
-                        'extended_valid_elements : \'img[src|alt|title|width|height|original-width]\',' +
-                        'media_poster: false,' +
-                        'height:\'180\',' +
-                        (isEditable ? '' : 'readonly: 1,') +
-                        'menubar:false}\"' +
-                        'ng-model=\"translation\" name=\"translation\">' +
-                        '</textarea>' +
-                        bottomMenuControls() +
-                        '</form>';
-                case 'icon':
-                    var icons = ['', 'adjust', 'anchor', 'archive', 'area-chart', 'arrows', 'arrows-h', 'arrows-v', 'asterisk', 'at', 'ban', 'bar-chart', 'barcode', 'bars', 'beer', 'bell',
-                        'bell-o', 'bell-slash', 'bell-slash-o', 'bicycle', 'binoculars', 'birthday-cake', 'bolt', 'bomb', 'book', 'bookmark', 'bookmark-o', 'briefcase', 'bug',
-                        'building', 'building-o', 'bullhorn', 'bullseye', 'bus', 'calculator', 'calendar', 'calendar-o', 'camera', 'camera-retro', 'car', 'caret-square-o-down', 'caret-square-o-left',
-                        'caret-square-o-right', 'caret-square-o-up', 'cc', 'certificate', 'check', 'check-circle', 'check-circle-o', 'check-square', 'check-square-o', 'child', 'circle', 'circle-o',
-                        'circle-o-notch', 'circle-thin', 'clock-o', 'cloud', 'cloud-download', 'cloud-upload', 'code', 'code-fork', 'coffee', 'cog', 'cogs', 'comment', 'comment-o', 'comments',
-                        'comments-o', 'compass', 'copyright', 'credit-card', 'crop', 'crosshairs', 'cube', 'cubes', 'cutlery', 'database', 'desktop', 'dot-circle-o', 'download', 'ellipsis-h',
-                        'ellipsis-v', 'envelope', 'envelope-o', 'envelope-square', 'eraser', 'exchange', 'exclamation', 'exclamation-circle', 'exclamation-triangle', 'external-link', 'external-link-square',
-                        'eye', 'eye-slash', 'eyedropper', 'fax', 'female', 'fighter-jet', 'file', 'file-o', 'file-text', 'file-text-o', 'file-archive-o', 'file-audio-o', 'file-code-o', 'file-excel-o',
-                        'file-image-o', 'file-pdf-o', 'file-powerpoint-o', 'file-video-o', 'file-word-o', 'film', 'filter', 'fire', 'fire-extinguisher', 'flag', 'flag-checkered', 'flag-o', 'flask',
-                        'folder', 'folder-o', 'folder-open', 'folder-open-o', 'frown-o', 'futbol-o', 'gamepad', 'gavel', 'gift', 'glass', 'globe', 'graduation-cap', 'hdd-o', 'headphones',
-                        'heart', 'heart-o', 'history', 'home', 'inbox', 'info', 'info-circle', 'key', 'keyboard-o', 'language', 'laptop', 'leaf', 'lemon-o', 'level-down', 'level-up',
-                        'life-ring', 'lightbulb-o', 'line-chart', 'location-arrow', 'lock', 'magic', 'magnet', 'male', 'map-marker', 'meh-o', 'microphone', 'microphone-slash', 'minus', 'minus-circle',
-                        'minus-square', 'minus-square-o', 'mobile', 'money', 'moon-o', 'music', 'newspaper-o', 'paint-brush', 'paper-plane', 'paper-plane-o', 'paw', 'pencil', 'pencil-square',
-                        'pencil-square-o', 'phone', 'phone-square', 'picture-o', 'pie-chart', 'plane', 'plug', 'plus', 'plus-circle', 'plus-square', 'plus-square-o', 'power-off', 'print', 'puzzle-piece',
-                        'qrcode', 'question', 'question-circle', 'quote-left', 'quote-right', 'random', 'recycle', 'refresh', 'reply', 'reply-all', 'retweet', 'road', 'rocket', 'rss', 'rss-square',
-                        'search', 'search-minus', 'search-plus', 'share', 'share-alt', 'share-alt-square', 'share-square', 'share-square-o', 'shield', 'shopping-cart', 'sign-in', 'sign-out', 'signal',
-                        'sitemap', 'sliders', 'smile-o', 'sort', 'sort-alpha-asc', 'sort-alpha-desc', 'sort-amount-asc', 'sort-amount-desc', 'sort-asc', 'sort-desc', 'sort-numeric-asc', 'sort-numeric-desc', 'space-shuttle',
-                        'spinner', 'spoon', 'square', 'square-o', 'star', 'star-half', 'star-half-o', 'star-o', 'suitcase', 'sun-o', 'tablet', 'tachometer', 'tag', 'tags', 'tasks', 'taxi', 'terminal',
-                        'thumb-tack', 'thumbs-down', 'thumbs-o-down', 'thumbs-o-up', 'thumbs-up', 'ticket', 'times', 'times-circle', 'times-circle-o', 'tint', 'toggle-off', 'toggle-on', 'trash', 'trash-o',
-                        'tree', 'trophy', 'truck', 'tty', 'umbrella', 'university', 'unlock', 'unlock-alt', 'upload', 'user', 'users', 'video-camera', 'volume-down', 'volume-off', 'volume-up',
-                        'wheelchair', 'wifi', 'wrench'];
-                    var iconTemplate = '<form name=\"i18nForm\" ng-submit=\"submit()\"><div class=\"icons-list\">';
-                    for (var i in icons) {
-                        iconTemplate += '<button ng-click=\"translation = \'fa-' + icons[i] + '\'\" title=\"' + icons[i] + '\" ng-class=\"{\'active\':translation == \'fa-' + icons[i] + '\'}\">' +
-                            '<i class=\"fa fa-' + icons[i] + ' fa-fw\"></i></button>';
-                    }
-                    iconTemplate += '</div>' +
-                        '<div class=\"dropdown-menu-buttons\">' +
-                        '<button type=\"reset\" class=\"btn btn-default inline\" ng-click=\"cancel()\" i18n code=\"i18n.menu.cancel.button\" default=\"Annuleren\" read-only ng-bind="var"></button>' +
-                        '</div></form>';
-                    return iconTemplate;
-                default:
-                    return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
-                        topMenuControls() +
-                        '<textarea name=\"translation\" rows=\"12\" ng-model=\"translation\" ' + (isEditable ? '' : 'disabled=\"true\"') + '></textarea>' +
-                        bottomMenuControls() +
-                        '</form>';
-            }
+    .run(['i18nRendererTemplateInstaller', 'ngRegisterTopicHandler', function (installer, ngRegisterTopicHandler) {
+        ngRegisterTopicHandler({
+            topic: 'edit.mode',
+            handler: installTemplates,
+            executeHandlerOnce: true
+        });
 
-            function topMenuControls() {
-                return '<div class=\"clearfix margin-bottom\" ng-show=\"followLink || locale\">' +
-                    '<button ng-if=\"followLink\" ng-disabled=\"i18nForm.$dirty\" type=\"button\" class=\"btn btn-primary pull-left\" ng-click=\"followLink()\" i18n code=\"i18n.menu.follow.link\" read-only ng-bind="var">' +
-                    '</button>' +
-                    '<span class=\"pull-right\" ng-if=\"locale\"><i class=\"fa fa-globe fa-fw\"></i> {{locale | toLanguageName}}</span>' +
-                    '</div>';
-            }
+        function installTemplates() {
+            installer.add('full', function (args) {
+                return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
+                    installer.topMenuControls() +
+                    '<textarea ui-tinymce=\"{' +
+                    'plugins: [\'link fullscreen textcolor paste table binartax.link\'],' +
+                    'toolbar: \'undo redo | styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent table | binartax.link | fullscreen\',' +
+                    'theme_advanced_resizing: true,' +
+                    'theme_advanced_resizing_use_cookie : false,' +
+                    'object_resizing: false,' +
+                    'height:\'180\',' +
+                    (args.isEditable ? '' : 'readonly: 1,') +
+                    'menubar:false}\"' +
+                    'ng-model=\"translation\" name=\"translation\">' +
+                    '</textarea>' +
+                    installer.bottomMenuControls(args.isEditable) +
+                    '</form>';
+            });
 
-            function bottomMenuControls() {
-                return '<div class=\"dropdown-menu-buttons\">' +
-                    (
-                        isEditable
-                            ? '<button type=\"reset\" class=\"btn btn-danger pull-left\" ng-click=\"erase()\" i18n code=\"i18n.menu.erase.text.button\" read-only ng-bind="var"></button>' +
-                        '<button type=\"submit\" class=\"btn btn-primary\" i18n code=\"i18n.menu.save.button\" read-only ng-bind="var"></button>' +
-                        '<button type=\"reset\" class=\"btn btn-default\" ng-click=\"cancel()\" i18n code=\"i18n.menu.cancel.button\" read-only ng-bind="var"></button>'
-                            : '<span class=\"pull-left margin-bottom\" i18n code=\"i18n.menu.no.multilingualism.message\" read-only><i class=\"fa fa-info-circle fa-fw\"></i> <span ng-bind="var"></span></span>' +
-                        '<button type=\"button\" class=\"btn btn-default\" ng-click=\"cancel()\" i18n code=\"i18n.menu.close.button\" read-only ng-bind="var"></button>'
-                    ) +
-                    '</div>';
-            }
-        })
+            installer.add('media', function (args) {
+                return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
+                    installer.topMenuControls() +
+                    '<textarea ui-tinymce=\"{' +
+                    'plugins: [\'fullscreen media paste\'],' +
+                    'toolbar: \'undo redo | media | fullscreen\',' +
+                    'theme_advanced_resizing: true,' +
+                    'theme_advanced_resizing_use_cookie : false,' +
+                    'object_resizing: false,' +
+                    'height:\'180\',' +
+                    (args.isEditable ? '' : 'readonly: 1,') +
+                    'menubar:false}\"' +
+                    'ng-model=\"translation\" name=\"translation\">' +
+                    '</textarea>' +
+                    installer.bottomMenuControls(args.isEditable) +
+                    '</form>';
+            });
+
+            installer.add('full-media', function (args) {
+                return '<form name=\"i18nForm\" ng-submit=\"submit()\">' +
+                    installer.topMenuControls() +
+                    '<textarea ui-tinymce=\"{' +
+                    'plugins: [\'link fullscreen binartax.img textcolor paste table binartax.link\'],' +
+                    'toolbar: \'undo redo | styleselect | bold italic | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent table | binartax.link | binartax.img | fullscreen\',' +
+                    'theme_advanced_resizing: true,' +
+                    'theme_advanced_resizing_use_cookie : false,' +
+                    'object_resizing: false,' +
+                    'extended_valid_elements : \'img[src|alt|title|width|height|original-width]\',' +
+                    'media_poster: false,' +
+                    'height:\'180\',' +
+                    (args.isEditable ? '' : 'readonly: 1,') +
+                    'menubar:false}\"' +
+                    'ng-model=\"translation\" name=\"translation\">' +
+                    '</textarea>' +
+                    installer.bottomMenuControls(args.editor) +
+                    '</form>';
+            });
+        }
     }])
     .run(['$rootScope', '$window', 'resourceLoader', 'activeUserHasPermission', 'topicMessageDispatcher', '$timeout', function ($rootScope, $window, resourceLoader, activeUserHasPermission, topicMessageDispatcher, $timeout) {
         activeUserHasPermission({
